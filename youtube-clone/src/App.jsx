80448fad280a624b84e3aef8388481b7eaa9beb3
Dhiraj104 <dhirajjaybhay1998@gmail.com>
2024-08-07 19:01:52 +0530

    Package install

---
+++ youtube-clone/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route>
+        
+      </Route>
+    </Routes>
+    </>
+  )
+}
+
+export default App
